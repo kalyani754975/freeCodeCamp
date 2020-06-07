dc5f6fb78bebd1ec0d042f5ec0e5dee2a3dfa017
Aakash Sharma <aa.sharma@outlook.com>
2020-06-07 11:59:55 +0530

    generating object with denom and number

--- Javascript Data Structures and Algorithms Project/Cash Register/app.js
+++ Javascript Data Structures and Algorithms Project/Cash Register/app.js
@@ -1,46 +1,72 @@
-/* 
-JavaScript Algorithms and Data Structures Projects: Cash Register
-Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
-
-cid is a 2D array listing available currency.
-
-The checkCashRegister() function should always return an object with a status key and a change key.
-
-Return {status: "INSUFFICIENT_FUNDS", change: []} if cash-in-drawer is less than the change due, or if you cannot return the exact change.
-
-Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for the key change if it is equal to the change due.
-
-Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key.
-
-Currency Unit	Amount
-Penny	$0.01 (PENNY)
-Nickel	$0.05 (NICKEL)
-Dime	$0.1 (DIME)
-Quarter	$0.25 (QUARTER)
-Dollar	$1 (ONE)
-Five Dollars	$5 (FIVE)
-Ten Dollars	$10 (TEN)
-Twenty Dollars	$20 (TWENTY)
-One-hundred Dollars	$100 (ONE HUNDRED)
-See below for an example of a cash-in-drawer array:
-
-[
-  ["PENNY", 1.01],
-  ["NICKEL", 2.05],
-  ["DIME", 3.1],
-  ["QUARTER", 4.25],
-  ["ONE", 90],
-  ["FIVE", 55],
-  ["TEN", 20],
-  ["TWENTY", 60],
-  ["ONE HUNDRED", 100]
-]
-
- */
-
 function checkCashRegister(price, cash, cid) {
-  var change;
-  return change;
+
+    // Calculate change
+    var temp_change = cash - price;
+    denomination = [100, 20, 10, 5, 1, 0.25, 0.10, 0.05, 0.01];
+
+    var i;
+    var result;
+    let cid_change = {
+        status: "",
+        change: []
+    };
+
+    for (i = 0; i < denomination.length; i++) {
+        result = temp_change / denomination[i];
+        if (result >= 1) {
+            switch (denomination[i]) {
+                case 100:
+                    denom = "ONE HUNDRED";
+                    break;
+                case 20:
+                    denom = "TWENTY";
+                    break;
+                case 10:
+                    denom = "TEN";
+                    break;
+                case 5:
+                    denom = "FIVE";
+                    break;
+                case 1:
+                    denom = "ONE";
+                    break;
+                case 0.25:
+                    denom = "QUARTER";
+                    break;
+                case 0.10:
+                    denom = "DIME";
+                    break;
+                case 0.05:
+                    denom = "NICKEL";
+                    break;
+                case 0.01:
+                    denom = "PENNY";
+                    break;
+            }
+            cid_change.change.push([denom, parseInt(result)]);
+            temp_change -= parseInt(result) * denomination[i];
+            result = temp_change;
+        }
+    }
+
+    cid[8][1]; // 100
+    Number_100 = cid[8][1] / 100;
+
+    cid[7][1]; // 60
+    Number_20 = cid[7][1] / 20;
+
+    cid[6][1]; // 20
+    cid[5][1]; // 55
+    cid[4][1]; // 90
+    cid[3][1]; // 4.25
+    cid[2][1]; // 3.1
+    cid[1][1]; // 2.05
+    cid[0][1]; // 1.01
+
 }
 
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+// checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+
+checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+
+// should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
