3fed6afe72dba72736752085b199229f8881830a
Kalyani <kalyani754975@gmail.com>
2020-06-30 16:33:51 +0530

    survey form

--- Responsive Web Design Certification/Responsive Web Design Projects/Survey Form/merge.js
+++ Responsive Web Design Certification/Responsive Web Design Projects/Survey Form/merge.js
@@ -1,25 +1,25 @@
-function fun(l,r){     //    l=[5] ,,,  r=[2]
-    var arr=[]          //  arr=
-    while(l.length && r.length){    // 1 && 1 
-      if (l[0]<r[0]){   //     5<2
-        arr.push(l.shift())
-      }
-      else{
-        arr.push(r.shift())     // arr=[2]   ,,,,,  r=[]
-      }
-    }
-  return arr.concat(l,r)
-  }  
-  // ========================================
-function mergeSort(arr) {   // arr=[30,24,58,12,70,10]
-    if (arr.length<2){     // false
-      return arr
-    }
-  var m=Math.floor((arr.length/2) )  //   m=3
-  var l=arr.slice(0,m)            //   l = [30,24,58]
-  
-  var r=arr.slice(m,)              //  r=[12,70,10]
-  return fun(mergeSort(l),mergeSort(r))   // 
-  }
-mergeSort([30,24,58,12,70,10]); 
 
+const permutator = (inputArr) => {       //  
+  let result = [];
+
+  const permute = (arr, m = []) => {         //  arr=["a","b"]   m=[]
+    if (arr.length === 0) {                 
+      result.push(m)
+    } else {
+      for (let i = 0; i < arr.length; i++) {          //  i=0  
+        let curr = arr.slice();                    //  
+      
+             
+        let next = curr.splice(i, 1);          
+      
+        
+        permute(curr.slice(), m.concat(next))   
+     }
+   }
+ }
+// 
+ permute(inputArr)          
+
+ return result;
+}
+permutator(["a"])    
