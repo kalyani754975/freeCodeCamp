65aa4747f00d2558c58482df0cebbb66e1c302aa
Kalyani <kalyani754975@gmail.com>
2020-07-02 14:04:10 +0530

    landing page

--- Responsive Web Design Certification/Responsive Web Design Projects/Survey Form/merge.js
+++ Responsive Web Design Certification/Responsive Web Design Projects/Survey Form/merge.js
@@ -1,25 +1,44 @@
 
-const permutator = (inputArr) => {       //  
-  let result = [];
+function fun1(d){         // d=28
+  if (d==1){
+    return 1
+  }   
+  var n=d          // n=28
+var i=2                // i=2  
+var p=1            
+// ====================
+  while(i*i<=d){        //   36<=28         
+    var c=1                   // c=1  
+    while(n%i==0){      //   
+      n=n/i         
+      c+=1        
+    }
+    i++         
+    p=p*c            //   p=3        n=7      
+  }       
+  // =======================
+if (d==n||n>1){   
+  p=p*2                 // 6 
 
-  const permute = (arr, m = []) => {         //  arr=["a","b"]   m=[]
-    if (arr.length === 0) {                 
-      result.push(m)
-    } else {
-      for (let i = 0; i < arr.length; i++) {          //  i=0  
-        let curr = arr.slice();                    //  
-      
-             
-        let next = curr.splice(i, 1);          
-      
-        
-        permute(curr.slice(), m.concat(next))   
-     }
-   }
- }
-// 
- permute(inputArr)          
-
- return result;
+  }     
+return p
 }
-permutator(["a"])    
+
+
+
+
+function divisibleTriangleNumber(x){
+var n=1
+var d=1
+
+while( fun1(d)<=x){
+  n++
+  d+=n
+
+}
+return d
+
+}
+
+
+divisibleTriangleNumber(28)    
