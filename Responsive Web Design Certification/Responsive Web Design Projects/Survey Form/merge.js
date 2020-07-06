d1a08ce6d4ca0785c5a8a4bc06f9ee39b0d5c575
Kalyani <kalyani754975@gmail.com>
2020-07-06 16:29:37 +0530

    landing-page

--- Responsive Web Design Certification/Responsive Web Design Projects/Survey Form/merge.js
+++ Responsive Web Design Certification/Responsive Web Design Projects/Survey Form/merge.js
@@ -1,44 +1,32 @@
+var getDigits = function(power) {         //   15  
+    var digits = [];                                     //  []
+    digits[0] = 1;                  //    [16]
+    
 
-function fun1(d){         // d=28
-  if (d==1){
-    return 1
-  }   
-  var n=d          // n=28
-var i=2                // i=2  
-var p=1            
-// ====================
-  while(i*i<=d){        //   36<=28         
-    var c=1                   // c=1  
-    while(n%i==0){      //   
-      n=n/i         
-      c+=1        
+    while (power > 0) {          
+        for (var i = digits.length-1; i >= 0; i--) {    // i=0  
+            digits[i] *= 2;                          //   1=1*2   =2
+            
+
+            if (digits[i] > 9) {                 
+                digits[i] -= 10;
+                digits[i+1] = digits[i+1] || 0;
+                digits[i+1] += 1;
+            }
+        }
+        power--;  
     }
-    i++         
-    p=p*c            //   p=3        n=7      
-  }       
-  // =======================
-if (d==n||n>1){   
-  p=p*2                 // 6 
+    return digits;
+};
 
-  }     
-return p
-}
-
-
-
-
-function divisibleTriangleNumber(x){
-var n=1
-var d=1
-
-while( fun1(d)<=x){
-  n++
-  d+=n
-
-}
-return d
-
-}
-
-
-divisibleTriangleNumber(28)    
+var powerDigitSum = function(power){              // 
+  
+    var digits = getDigits(power); 
+                        
+    var sum = 0;
+    for(var i = digits.length - 1; i >= 0; i--){
+        sum += digits[i];
+    }
+    return sum;
+};
+powerDigitSum(15);  
