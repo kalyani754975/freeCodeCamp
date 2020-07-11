efa192ca63649565edabc5aa4a0415fb3a68449c
Kalyani <59971683+kalyani754975@users.noreply.github.com>
2020-07-11 13:35:00 +0530

    Update binary-to decimal.js

--- Interview Prep/binary-to decimal.js
+++ Interview Prep/binary-to decimal.js
@@ -1,16 +1,14 @@
 function decimal(num) {
-    while(num > 0){
-    var final = num%10
-    var sum = 0
-   var bin = 1;
-    var mult = 0;
-    var bin = final*2**mult
-     
-     
-    num = (num/10)>>0
+  var mult = 0;
+  var sum = 0;
+  var bin = 1;
+  while (num > 0) {
+    var final = num % 10
+    var bin = final * 2 ** mult
+    num = (num / 10) >> 0
     sum += bin
     mult++
-    }
-    return sum;
-     }
-     decimal(111)
+  }
+  return sum;
+}
+console.log(decimal(111))
