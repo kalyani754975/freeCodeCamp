ea6aa39e0ae801233563076cb2e084c968d80301
Kalyani <59971683+kalyani754975@users.noreply.github.com>
2020-07-14 11:10:06 +0530

    Create remove-element-from-a-linked-list.js

---
+++ Interview Prep/Data Structures/remove-element-from-a-linked-list.js
@@ -0,0 +1,51 @@
+function LinkedList() {
+  var length = 0;
+  var head = null;
+
+  var Node = function(element){
+    this.element = element;
+    this.next = null;
+  };
+
+  this.size = function(){
+    return length;
+  };
+
+  this.head = function(){
+    return head;
+  };
+
+  this.add = function(element){
+    var node = new Node(element);
+    if(head === null){
+        head = node;
+    } else {
+        var currentNode = head;
+
+        while(currentNode.next){
+            currentNode  = currentNode.next;
+        }
+
+        currentNode.next = node;
+    }
+
+    length++;
+  };
+
+  this.remove = function(element){
+    var currentnode = head;
+    var prev = null;
+    while(currentnode != null){
+      if(element == currentnode.element){
+        if(prev == null){
+          head = currentnode.next
+        } else {
+          prev.next = currentnode.next
+        }
+        length--
+      }
+      prev = currentnode
+      currentnode = currentnode.next
+    }
+  };
+}
